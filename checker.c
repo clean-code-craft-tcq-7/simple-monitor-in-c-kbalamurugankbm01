81a76a47746162477bad25862358ce48a892dd82
kbalamurugankbm01 <115966525+kbalamurugankbm01@users.noreply.github.com>
2022-12-12 21:02:36 +0530

    improvement

--- checker.c
+++ checker.c
@@ -8,9 +8,8 @@ int batteryIsOk(float temperature, float soc, float chargeRate);
 int batteryIsOk(float temperature, float soc, float chargeRate) {
   int batteryIsOk_bool = 0;
 
-  batteryIsOk_bool = batteryTemperatureIsOk(temperature);
-  batteryIsOk_bool &&= batterySOCOk(soc);
-  batteryIsOk_bool &&= batteryChargeRateOk(chargeRate);
+  batteryIsOk_bool = batteryTemperatureIsOk(temperature) && batterySOCOk(soc) \
+  && batteryChargeRateOk(chargeRate);
 
   return batteryIsOk_bool;
 }
@@ -36,18 +35,27 @@ int batteryChargeRateOk(float chargeRate) {
   return 1;
 }
 
-int printBatteryChargeRateWarning(){
-  printf("Charge Rate out of range!\n");
+int printBatteryChargeRateWarning(float chargeRate){
+  if (batteryChargeRateOk(chargeRate) == 0) {
+    printf("Charge Rate out of range!\n");
+  }
+  
 }
 
 
-int printBatteryTemperatureWarning(){
-  printf("Temperature out of range!\n");
+int printBatteryTemperatureWarning(float temperature){
+  if(batteryTemperatureIsOk(temperature) == 0){
+    printf("Temperature out of range!\n");
+  }
+  
 }
 
 
-int printBatterySOCWarning(){
-  printf("State of Charge out of range!\n");
+int printBatterySOCWarning(float soc){
+  if(batterySOCOk(soc) == 0){
+    printf("State of Charge out of range!\n");
+  }
+
 }
 
 int main() {
